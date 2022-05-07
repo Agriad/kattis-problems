81ed994de9e692abe3f979870ae4746a3a8c65e4
Justin Arieltan <agriad1@yahoo.com>
2022-05-07 15:07:03 +0200

    add workinggroup 1 birthday memorization

--- birthday-memorization/birthday-memorization.go
+++ birthday-memorization/birthday-memorization.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -16,28 +19,12 @@ func main() {
 	var inputs []string
 	var input string
 
+	reader := bufio.NewReader(os.Stdin)
+
 	for i := 0; i < inputAmountString; i++ {
-		fmt.Scanln(&input)
+		input, _ = reader.ReadString('\n')
 		inputs = append(inputs, input)
-		fmt.Println("i")
 	}
-
-	// fmt.Println("Hello, World!")
-
-	// var inputs [3]string = [3]string{"Sanna 1 16/03", "Simon 2 16/03", "Saga 3 14/10"}
-	// var inputAmount int = 3
-
-	// var inputs [10]string = [10]string{"Oden 78 03/12",
-	// 	"Tor 132 14/05",
-	// 	"Freja 10000 14/05",
-	// 	"Loke 512 12/10",
-	// 	"Hel 14 04/05",
-	// 	"Fjorgynn 532 13/05",
-	// 	"Hildegun 500 13/05",
-	// 	"Vindsval 17 03/12",
-	// 	"Snotra 20 04/05",
-	// 	"Kvaser 420 03/12"}
-	// var inputAmount int = 10
 
 	var parsedInput []string
 
@@ -65,12 +52,31 @@ func main() {
 		}
 	}
 
+	sort.Strings(parsedInput)
+
 	fmt.Println(len(parsedInput))
 
 	for i := 0; i < len(parsedInput); i++ {
 		fmt.Println(parsedInput[i])
 	}
 }
+
+// test input
+
+// var inputs [3]string = [3]string{"Sanna 1 16/03", "Simon 2 16/03", "Saga 3 14/10"}
+// var inputAmount int = 3
+
+// var inputs [10]string = [10]string{"Oden 78 03/12",
+// 	"Tor 132 14/05",
+// 	"Freja 10000 14/05",
+// 	"Loke 512 12/10",
+// 	"Hel 14 04/05",
+// 	"Fjorgynn 532 13/05",
+// 	"Hildegun 500 13/05",
+// 	"Vindsval 17 03/12",
+// 	"Snotra 20 04/05",
+// 	"Kvaser 420 03/12"}
+// var inputAmount int = 10
 
 // https://open.kattis.com/problems/fodelsedagsmemorisering
 // https://go.dev/doc/tutorial/getting-started
